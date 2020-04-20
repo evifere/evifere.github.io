8bab7432723d869d5e1d935593021d11bddf5f3e
JOLLY <evifere@gmail.com>
2020-04-20 10:58:51 +0200

    set react_app_mode and public_url

--- n1c0l4s-j0117/service-worker.js
+++ n1c0l4s-j0117/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.56ebfe37febd160e73be929f7dfff811.js"
+  "/n1c0l4s-j0117/precache-manifest.5910012b54fe55c6ea642d52a7605510.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/n1c0l4s-j0117/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
